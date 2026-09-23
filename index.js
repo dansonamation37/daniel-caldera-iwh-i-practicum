7f4fa418321264438578f78ae50c1e2c44e2d548
dansonamation37 <daniel@sonamation.com>
2026-09-23 13:33:34 -0700

    Add POST route to create custom object records

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ app.get('/update-cobj', (req, res) => {
     title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
   });
 });
+
+app.post('/update-cobj', async (req, res) => {
+  const newRecord = {
+    properties: {
+      name: req.body.name,
+      performer_name: req.body.performer_name,
+      genre: req.body.genre,
+      audience_size: req.body.audience_size
+    }
+  };
+  try {
+    await axios.post(`https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE}`, newRecord, { headers });
+    res.redirect('/');
+  } catch (error) {
+    console.error(error.response ? error.response.data : error.message);
+    res.status(500).send('Error creating record');
+  }
+});
 //
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
